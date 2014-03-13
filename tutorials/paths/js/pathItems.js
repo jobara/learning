a75aeb217cfe1cf8e5713e81827e3b36df4041a5
Justin Obara <obara.justin@gmail.com>
2014-03-13 10:20:36 -0400

    Added path segments and smoothing tutorials

---
+++ tutorials/paths/js/pathItems.js
@@ -0,0 +1,40 @@
+var demo = demo || {};
+
+(function (demo, $) {
+    demo.initSegments = function (elm) {
+        var canvas = $(elm);
+        paper.setup(canvas[0]);
+
+        var myPath = new paper.Path();
+        myPath.strokeColor = 'black';
+        myPath.add(new paper.Point(0, 0), new paper.Point(100, 50));
+
+        // insert a segment between the two existing
+        // segments in the path:
+        myPath.insert(1, new paper.Point(30, 40));
+    };
+
+    demo.initSmoothing = function (elm) {
+        var canvas = $(elm);
+        paper.setup(canvas[0]);
+
+        var path = new paper.Path();
+        path.strokeColor = 'black';
+        path.add(new paper.Point(30, 75));
+        path.add(new paper.Point(30, 25));
+        path.add(new paper.Point(80, 25));
+        path.add(new paper.Point(80, 75));
+        path.closed = true;
+
+        // Select the path, so we can see its handles:
+        path.fullySelected = true;
+
+        // Create a copy of the path and move it 100pt to the right:
+        var copy = path.clone();
+        copy.fullySelected = true;
+        copy.position.x += 100;
+
+        // Smooth the segments of the copy:
+        copy.smooth();
+    };
+})(demo, jQuery);
